4a2e60c2d7170e46c4db8ec86faff1a8ec306a3a
Raghul Jagannathan <raghulj@gmail.com>
2013-07-19 14:05:39 +0800

    Added a base login for add

---
+++ logix.py
@@ -0,0 +1,67 @@
+
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+from nltk.classify import PositiveNaiveBayesClassifier
+import nltk.data
+from nltk.tokenize import *
+
+
+sent_detector = nltk.data.load('english.pickle')
+
+
+
+CATEGORY = {}
+
+CATEGORY['FURN'] = 'Not furnished'
+CATEGORY['ELEC'] = 'No aircon'
+CATEGORY['FURN'] = 'partially furnished'
+CATEGORY['UTEN'] = 'No Cooking'
+CATEGORY['WIFI'] = 'No wifi'
+CATEGORY['FURN'] = 'Unfurnished'
+
+PRICE_HIGH = 2000
+PRICE_MID = 1000
+
+def get_price_range(price):
+	if price >= 2000:
+		return "H"
+	elif price > 1000 and price < 2000:
+		return "M"
+	elif price < 1000:
+		return "L"
+
+validators = [
+		'No aircon',
+		'Not furnished',
+		'partially furnished',
+		'No Cooking',
+		'No wifi',
+		'Unfurnished'
+]
+
+
+def analyse(description_text):
+	various_sentences= regexp_tokenize(description_text, r'[,\.\?!"\n\r]\s*', gaps=True)
+	suggested_data = {}
+
+	for sent in various_sentences:
+		for valid in validators:
+			rate  = fuzz.ratio(valid, sent)
+			if rate > 50:
+				suggested_data[valid] = rate
+	return suggested_data
+
+def get_suggestion(value):
+	for k, v in CATEGORY.iteritems():
+		if v == value:
+			return k
+
+def get_ad_category(description):
+	suggested_data = analyse(description)
+	value1 = max(suggested_data, key=suggested_data.get)
+	cat =  get_suggestion(value1)
+	if cat:
+		return cat
+	else:
+		return "MISC"
--- smart_ad.py
+++ smart_ad.py
@@ -1,27 +1,46 @@
 from flask import Flask, jsonify
 import urllib2
 import simplejson as json
+from logix import *
 
 app = Flask(__name__)
 app.debug = True
 
 
-def get_smart_ad():
-	data = {}
-	arr = []
-	data['title'] = "Some detail"
-	data['image'] = "http://www.freegreatdesign.com/icon/large-apple-icon-png-2921"
-	data['summary'] = "Please buy our product"
-	arr.append(data)
-	return arr
+def get_smart_ad(category, price_range):
+	if category and price_range:
+		data = {}
+		arr = []
+		data['title'] = "Some detail"
+		data['image'] = "http://www.freegreatdesign.com/icon/large-apple-icon-png-2921"
+		data['summary'] = "Please buy our product"
+		arr.append(data)
+		return arr
 
 
 @app.route('/listing_detail/<listing_id>')
 def smart_detail(listing_id):
 	url =  'http://api.propertyguru.com.sg/listing/view/%s' % listing_id
-	print url
 	response = urllib2.urlopen(url)
 	html = response.read()
 	dd = json.loads(html)
-	dd['advertisements'] = get_smart_ad()
+	try:
+		if dd['price'] and dd['description']:
+			price_range = get_price_range(dd['price'])
+			category = get_ad_category(dd['description'])
+			print price_range + "~~" + category
+			ad = get_smart_ad(category, price_range)
+			if ad:
+				dd['advertisements'] = ad
+	except:
+		## jsut ignore for poc
+		print 
 	return jsonify(dd)
+
+
+
+# print get_price_range(4000)
+# print get_price_range(1500)
+# print get_price_range(400)
+
+# print get_ad_category(descrip)
